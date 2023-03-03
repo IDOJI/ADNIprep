8bb673637324a472aeb089ec99effaf092c94f2f
Ido Ji <ido.ji@proton.me>
2023-03-03 16:19:52 +0900

    FSL

---
+++ R/RS.fMRI_FSL.R
@@ -0,0 +1,45 @@
+# Load necessary packages
+library(oro.nifti)
+
+# Set input and output filenames
+input <- "resting_state.nii.gz"
+output <- "resting_state_preprocessed.nii.gz"
+
+# Motion correction
+system(paste("mcflirt -in", input, "-out", "rest_mc.nii.gz -plots -rmsrel -rmsabs"))
+
+# Slice timing correction
+system(paste("slicetimer -i rest_mc.nii.gz -o rest_stc.nii.gz --tcustom=custom.txt"))
+
+# Brain extraction
+system(paste("bet rest_stc.nii.gz rest_brain.nii.gz -F"))
+
+# Registration to MNI space
+system(paste("flirt -in rest_brain.nii.gz -ref MNI152_T1_2mm_brain.nii.gz -omat rest2mni.mat -dof 12"))
+
+# Normalization
+system(paste("fnirt --in=rest_brain.nii.gz --aff=rest2mni.mat --cout=rest2mni_warp --ref=MNI152_T1_2mm.nii.gz --iout=rest_norm.nii.gz"))
+
+# Smoothing
+system(paste("fslmaths rest_norm.nii.gz -s 6 rest_smooth.nii.gz"))
+
+# Bandpass filtering
+system(paste("fslmaths rest_smooth.nii.gz -bptf 0.01 0.1 rest_filtered.nii.gz"))
+
+# Save preprocessed data as NIfTI file
+system(paste("fslchfiletype NIFTI_GZ rest_filtered.nii.gz", output))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ R/RS.fMRI_Generate.Simulation.Data.R
@@ -0,0 +1,38 @@
+# Load necessary packages
+library(oro.nifti)
+library(neuRosim)
+
+# Set simulation parameters
+n_voxels <- 10000
+n_TR <- 200
+tr_duration <- 2
+noise_level <- 0.1
+signal_freqs <- c(0.01, 0.03)
+
+# Generate noise and signal time courses
+noise <- rnorm(n_voxels * n_TR, sd = noise_level)
+signal <- apply(sin(2 * pi * tr_duration * outer(1:n_TR, signal_freqs, "*")), 2, sum)
+
+# Combine noise and signal into fMRI time course
+fMRI <- matrix(0, nrow = n_voxels, ncol = n_TR)
+fMRI[] <- noise + signal
+
+# Save fMRI data as NIfTI file
+fMRI_array <- array(fMRI, dim = c(91, 109, 91, n_TR))
+fMRI_nii <- nifti(fMRI_array, type = "float")
+
+dim(fMRI_nii) <- list(91, 109, 91, n_TR)
+
+attributes(fMRI_nii)$pixdim <- c(2, 2, 2, tr_duration)
+
+fMRI_nii$intent_code <- 1007
+fMRI_nii$intent_name <- "NIFTI_INTENT_CONNECTIVITY_ESTIMATE"
+oro.nifti::writeNIfTI(fMRI_nii, filename = "resting_state_simulated.nii.gz")
+
+
+
+
+length(fMRI_nii)
+
+
+
