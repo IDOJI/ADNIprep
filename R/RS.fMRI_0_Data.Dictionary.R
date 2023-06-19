2f92ba39c18d76df0441e8598b3616bec3791c11
Ido Ji <ido.ji@proton.me>
2023-06-19 10:18:26 +0900

    RS fmri

--- R/RS.fMRI_0_Data.Dictionary.R
+++ R/RS.fMRI_0_Data.Dictionary.R
@@ -3,7 +3,7 @@ RS.fMRI_0_Data.Dictionary = function(colname, path_Data.Dic = "/Users/Ido/Librar
   # install.packages("C:/Users/lleii/Dropbox/Github/Rpkgs/ADNIprep/ADNIMERGE/ADNIMERGE_0.0.1.tar.gz", repose = NULL, type="source")
   # install.packages("/Users/Ido/Library/CloudStorage/Dropbox/Github/Rpkgs/ADNIprep/ADNIMERGE/ADNIMERGE_0.0.1.tar.gz")
   # require(ADNIMERGE)
-  dic = read.csv("/Users/Ido/Library/CloudStorage/Dropbox/Github/Rpkgs/ADNIprep/ADNIMERGE/DATADIC_03Jun2023.csv")
+  dic = read.csv(path_Data.dic)
 
   colname = toupper(colname)
   selected_col = dic %>% filter(FLDNAME == colname)
